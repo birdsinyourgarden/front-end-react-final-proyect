372d0fe710f6dae535771e9703247e24addc5986
Sierriconvoz <sierri.peri@gmail.com>
2023-03-07 14:42:33 +0100

    Header improces

--- src/components/LocalNavbar.jsx
+++ src/components/LocalNavbar.jsx
@@ -6,12 +6,13 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import "rsuite/dist/rsuite.css";
+import '../components/LocalNavbar.css';
 
 function LocalNavbar() {
   return (
     <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
       <Container>
-        <Navbar.Brand href="#home">Aidei Gestion Vacaciones y Ausencias App</Navbar.Brand>
+        <Navbar.Brand href="#home"><img src="./public/Aidei.png" alt="logo Aidei"></img></Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
