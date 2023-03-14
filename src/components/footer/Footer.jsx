7a2cbe4fa7f9c3f051dab64582c38b62e3667fa6
birdsinyourgarden <ruizpaloma9@gmail.com>
2023-03-14 12:35:09 +0100

    footer fixed

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <Card>    
       <footer>
-        <div class="container d-flex justify-content-between align-items-center">
+        <div class="d-flex justify-content-between align-items-center">
           <img src="./public/Copyright.png" alt="Copyright"></img>
           <img src="./public/femCoders.png" alt="logo Fem Coders"></img>
         </div>
