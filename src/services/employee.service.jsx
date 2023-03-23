f93e3100d1fcc002a117836bc7fa021b0fc32c51
unknown <camilaruiz17@gmail.com>
2023-03-23 13:23:40 +0100

    connected employee record

--- src/services/employee.service.jsx
+++ src/services/employee.service.jsx
@@ -3,5 +3,11 @@ import axios from "axios";
 const baseUrl = import.meta.env.VITE_BACKEND_URL;
 
 export const  registerEmployee = (data) => {
-    return axios.post(baseUrl + "/registerEmployee", data);
+    const token = localStorage.getItem("token");
+   const config = {
+      headers: {
+         Authorization: `Bearer ${token}`,
+      },
+   }
+    return axios.post(baseUrl + "/registerEmployee", data, config);
  };
--- src/views/employees/Employees.jsx
+++ src/views/employees/Employees.jsx
@@ -13,6 +13,7 @@ const  Employees= () => {
    } = useForm();
 
    const customSubmit = (data) => {
+      console.log('hjfjhgf')
       const formData = new FormData();
       formData.append('name', data.name);
       formData.append("surname", data.surname);
@@ -31,17 +32,14 @@ const  Employees= () => {
       response => {
          console.log ("datalol", response)
       }
-      ).catch(
-      err => {
-          // Mostrar un mensaje al usuario
-         console.log(err)
-         setLoginError(true)
-         setTimeout(() => {
-            setLoginError(false)
-         }, 3000);
-      }
-      )
+      ).catch(e => {
+         console.log (e)
+      })
    };
+
+   const onErrors = (e) => {
+      console.log('error', e)
+   }
 
    return (
       <Container className="p-5">
@@ -50,38 +48,38 @@ const  Employees= () => {
                Registro de Empleados
             </Card.Title>
             <Card.Body>
-            <Form  onSubmit={handleSubmit(customSubmit)}>
+            <Form  onSubmit={handleSubmit(customSubmit, onErrors)}>
                <Form.Group className="mb-3" controlId="employeeName">
                   <Form.Label>Nombre</Form.Label>
                   <input
-              type="text"
-              placeholder="Escribe el Nombre"
-              className='form-control shadow'
-              {...register("name", {
-                          required: true,
-                          maxLength: 30,
+            type="text"
+            placeholder="Escribe el Nombre"
+            className='form-control shadow'
+            {...register("name", {
+                        required: true,
+                        maxLength: 30,
                         })}/>
                </Form.Group>
                <Form.Group className="mb-3" controlId="employeeSurName">
                   <Form.Label>Apellidos</Form.Label>
                   <input
-              type="text"
-              placeholder="Escribe los Apellidos"
-              className='form-control shadow'
-              {...register("surname", {
-                          required: true,
-                          maxLength: 42,
+            type="text"
+            placeholder="Escribe los Apellidos"
+            className='form-control shadow'
+            {...register("surname", {
+                        required: true,
+                        maxLength: 42,
                         })}/>
                </Form.Group>
                <Form.Group className="mb-3" controlId="employeeEmail">
                   <Form.Label>Correo</Form.Label>
                   <input
-              type="email"
-              placeholder="Escribe el Correo"
-              className='form-control shadow'
-              {...register("email", {
-                          required: true,
-                          maxLength: 42,
+            type="email"
+            placeholder="Escribe el Correo"
+            className='form-control shadow'
+            {...register("email", {
+                        required: true,
+                        maxLength: 42,
                         })}/>
                </Form.Group>
                <Form.Group className="mb-3" controlId="password">
@@ -92,55 +90,52 @@ const  Employees= () => {
             className='form-control shadow'
             {...register("password", {
                         required: true,
-                        minLength: 5,
-                        maxLength: 200,
+                        minLength: 4,
+                        maxLength: 15,
                         })}/>
                </Form.Group>
                <Form.Group className="mb-3" controlId="confirmPassword">
                   <Form.Label>Confirmar Password</Form.Label>
                   <input
-              type="password"
-              placeholder="Vuelve a escribir la contraseña"
-              className='form-control shadow'
-              {...register("password", {
-                          required: true,
-                          minLength: 5,
-                          maxLength: 200,
+            type="password"
+            placeholder="Vuelve a escribir la contraseña"
+            className='form-control shadow'
+            {...register("password2", {
+                        required: true,
+                        minLength: 4,
+                        maxLength: 15,
                         })}/>
                </Form.Group>
                <Form.Group className="mb-3" controlId="employeePhone">
                   <Form.Label>Teléfono</Form.Label>
                   <input
-              type="text"
-              placeholder="Escribe un teléfono"
-              className='form-control shadow'
-              {...register("phone", {
-                          required: true,
-                          minLength: 5,
-                          maxLength: 200,
+            type="number"
+            placeholder="Escribe un teléfono"
+            className='form-control shadow'
+            {...register("phone", {
+                        required: true,
+                        minLength: 9,
+                        maxLength: 15,
                         })}/>
                </Form.Group>
                <Form.Group className="mb-3" controlId="employeeDni">
                   <Form.Label>DNI/NIE</Form.Label>
                   <input
-              type="text"
-              placeholder="DNI/NIE"
-              className='form-control shadow'
-              {...register("idNumber", {
-                          required: true,
-                          minLength: 5,
-                          maxLength: 200,
+            type="text"
+            placeholder="DNI/NIE"
+            className='form-control shadow'
+            {...register("idNumber", {
+                        required: true,
+                        minLength: 5,
+                        maxLength: 10,
                         })}/>
                </Form.Group>
-               <Form.Group 
-               className='form-control shadow'
+               <Form.Group>
+               <Form.Label>Sector</Form.Label>
+               <Form.Select className="mb-3 form-control shadow" controlId="employeeSector"
                {...register("sector", {
                            required: true,
-                           minLength: 5,
-                           maxLength: 200,
                         })}>
-               <Form.Label>Sector</Form.Label>
-               <Form.Select className="mb-3" controlId="employeeSector" >
                <option value="Jardinería">Jardinería</option>
                <option value="Limpieza Málaga">Limpieza Málaga</option>
                <option value="Limpieza Cádiz">Limpieza Cádiz</option>
@@ -149,46 +144,75 @@ const  Employees= () => {
                   </Form.Group>
                   <Form.Group className="mb-3" controlId="employeePhoto">
                   <Form.Label>Foto</Form.Label>
-                  <Form.Control type="text" placeholder="Imagen del Empleado" 
-                  	/>
+                  <input
+            type="file"
+            placeholder=""
+            className='form-control shadow'
+            {...register("image", {
+               required: true,
+               })}/>
+               {errors.image?.type === "required" && (
+               <small className="fail">The field cannot be empty</small>
+            )}
+            {errors.image?.type === "maxLength" && (
+               <small className="fail">Maximum characters are eight</small>
+            )}
                </Form.Group>
                <Form.Group className="mb-3" controlId="employeeStarting">
                   <Form.Label>Inicio del Contrato</Form.Label>
                   <input
-                     type="date"
-                     name="party"
-                     min="2010-04-01"
-                     max="2024-04-20"
-                     required />
+                  type="date"
+                  name="party"
+                  required
+                  {...register("startingDate", {
+                     min: "2010-04-01",
+                     max:"2024-04-20",
+                     required: true,
+                     })}
+                  />
                <span class="validity"></span>
                </Form.Group>
                <Form.Group className="mb-3" controlId="employeeFinished">
                   <Form.Label>Fin del Contrato</Form.Label>
                   <input
-                     type="date"
-                     name="party"
-                     min="2010-04-01"
-                     max="2024-04-20"
-                     required />
+                  type="date"
+                  name="party"
+                  required
+                  {...register("endingDate", {
+                     min: "2010-04-01",
+                     max:"2024-04-20",
+                     required: true,
+                     })}
+                  />
                <span class="validity"></span>
                </Form.Group>
                <Form.Group>
-               <Form.Label>Estatus del Empleado</Form.Label>
-               <Form.Select className="mb-3" controlId="employeeStatus">
+            
+               <Form.Label>Estado del Empleado</Form.Label>
+               <Form.Select className=" form-control shadow mb-3" controlId="employeeStatus"
+               {...register("status", {
+                           required: true,
+                        })}>
                <option value="true">Activo</option>
                <option value="false">Baja</option>
                   </Form.Select>
                   </Form.Group>
                <Form.Group>
                <Form.Label>Tipo de Contrato del Empleado</Form.Label>
-               <Form.Select className="mb-3" controlId="employeeType">
+               <Form.Select className="mb-3 form-control shadow" controlId="employeeType"
+               {...register("contractType", {
+                           required: true,
+                        })}>
                <option value="true">Permanente</option>
                <option value="false">Temporal</option>
                   </Form.Select>
                   </Form.Group>
                   <Form.Group>
                <Form.Label>Es Administrador?</Form.Label>
-               <Form.Select className="mb-3" controlId="employeeAuth">
+               <Form.Select className="mb-3 form-control shadow" controlId="employeeAuth"
+                  {...register("isAdmin", {
+                              required: true,
+                           })}>
                <option value="false">No</option>
                <option value="true">Si</option>
                   </Form.Select>
