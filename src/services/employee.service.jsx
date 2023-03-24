987bf01ed5ea0810e9536bee9172f1362df4cbbc
unknown <camilaruiz17@gmail.com>
2023-03-24 14:29:07 +0100

    show employee in the sidebar

--- src/services/employee.service.jsx
+++ src/services/employee.service.jsx
@@ -11,3 +11,13 @@ export const  registerEmployee = (data) => {
    }
     return axios.post(baseUrl + "/registerEmployee", data, config);
  };
+
+ export const getEmployees = () => {
+    const token = localStorage.getItem("token");
+    const config = {
+    headers: {
+        Authorization: `Bearer ${token}`,
+    },
+    }
+    return axios.get(baseUrl + "/employees", config);
+};
