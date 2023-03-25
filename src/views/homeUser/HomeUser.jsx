8f5c63bec30729664e780f724ac99a8ac9b26072
birdsinyourgarden <ruizpaloma9@gmail.com>
2023-03-25 22:43:42 +0100

    indentation

--- src/views/homeUser/HomeUser.jsx
+++ src/views/homeUser/HomeUser.jsx
@@ -1,16 +1,16 @@
 import SenderHolidays from "../../components/senderHolidays/SenderHolidays";
 import SenderAbsences from "../../components/senderAbsences/SenderAbsences";
-import "../homeUser/HomeUser.css"
+import "../homeUser/HomeUser.css";
 
 function HomeUser() {
-    return (
-        <div className="container">
-            <div className="wrap">
-                <SenderHolidays />
-                <SenderAbsences />    
-            </div>
-        </div>
-    )
+  return (
+    <div className="container">
+      <div className="wrap">
+        <SenderHolidays />
+        <SenderAbsences />
+      </div>
+    </div>
+  );
 }
 
 export default HomeUser;
