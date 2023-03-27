b03395a2489d00cf1ff8b6b3c83030d56495e612
ZLoln <lolanavalo@gmail.com>
2023-03-27 11:37:38 +0200

    Profile layout on progress

--- src/views/profile/Profile.jsx
+++ src/views/profile/Profile.jsx
@@ -5,60 +5,51 @@ const  Profile= () => {
     return (
         <Container className="p-5">
             <Card id="profileCard" className="text-start">
-                <Card.Title className="text-center">
-                <Form>
-                    <Form.Group className="mb-3" controlId="employeePhoto">
-                        <Form.Label>Foto</Form.Label>
-                        <Form.Control type="text" placeholder="Imagen del Empleado" />
-                    </Form.Group>
-                    <Form.Label>Nombre completo</Form.Label>
-                        <p>Matilda Pepote Popeye</p>
-                        <Form.Group>
-                            <Form.Label>Estatus del Empleado</Form.Label>
-                        </Form.Group>
-                        <Form.Group>
-                            <Form.Label>Tipo de Contrato del Empleado</Form.Label>
-                        </Form.Group>
-                </Form>
-                
+                <Card.Title id="profileHeader" className="text-center">
+                    <div id="photoContainer">
+                        <img id="employPhoto" src="https://res.cloudinary.com/dkbwmuo7n/image/upload/v1679689850/sierri.jpg" alt="" />
+                    </div>
+                    <div id="detailsContainer">
+                        <div id="nameDetails">
+                            <p>Nombre: Maria Pepote Popeye</p>
+                        </div>
+                        <div>
+                            <p>Estatus actual: Habilitado</p>
+                        </div>
+                        <div>
+                            <p>Tipo de Contrato: Permanente</p>
+                        </div>
+                    </div>
                 </Card.Title>
             <Card.Body id="profileDetails">
             <Form>    
                 <Form.Group className="mb-3" controlId="employeeDni">
-                    <Form.Label>DNI/NIE</Form.Label>
-                    <Form.Control type="text" placeholder="DNI o NIE del empleado" />
-                </Form.Group>  
+                    <Form.Label id="fieldTitle">DNI/NIE</Form.Label>
+                    <p>25587562P</p>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="employeeDni">
+                    <Form.Label id="fieldTitle">Sector</Form.Label>
+                    <p>Jardinería</p>
+                </Form.Group>    
                 <Form.Group className="mb-3" controlId="employeeStarting">
-                    <Form.Label>Inicio del Contrato</Form.Label>
-                        <input
-                            type="date"
-                            name="party"
-                            min="2010-04-01"
-                            max="2024-04-20"
-                            required />
-                        <span class="validity"></span>
+                    <Form.Label id="fieldTitle">Inicio del Contrato</Form.Label>
+                    <p>01/01/2023</p>
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="employeeFinished">
-                    <Form.Label>Fin del Contrato</Form.Label>
-                        <input
-                            type="date"
-                            name="party"
-                            min="2010-04-01"
-                            max="2024-04-20"
-                            required />
-                        <span class="validity"></span>
+                    <Form.Label id="fieldTitle">Fin del Contrato</Form.Label>
+                    <p>01/05/2023</p>
                 </Form.Group>    
                 <Form.Group className="mb-3" controlId="employeeEmail">
-                    <Form.Label>Correo</Form.Label>
-                    <Form.Control type="email" placeholder="Correo del empleado" />
+                    <Form.Label id="fieldTitle">Correo</Form.Label>
+                    <p>jdjd@gg.com</p>
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="password">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Label id="fieldTitle">Password</Form.Label>
+                    <p>kdkjlds_2255</p>
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="employeePhone">
-                    <Form.Label>Telefono</Form.Label>
-                    <Form.Control type="text" placeholder="Telefono del empleado" />
+                    <Form.Label id="fieldTitle">Telefono</Form.Label>
+                    <p>665955558</p>
                 </Form.Group>
             </Form>
             </Card.Body>
