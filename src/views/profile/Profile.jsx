643a01c165063678469da0387d1fc106c2d706dd
ZLoln <lolanavalo@gmail.com>
2023-03-27 14:21:24 +0200

    still working on layout and responsive

--- src/views/profile/Profile.jsx
+++ src/views/profile/Profile.jsx
@@ -3,7 +3,11 @@ import "../profile/Profile.css";
 
 const  Profile= () => {
     return (
+
         <div className="profileWrapper">
+            <div>
+                <p className="profTitle">Mi Perfil</p>
+            </div>
             <Container id="profileTemplate" className="p-5">
                 <Card id="profileCard" className="text-start">
                     <Card.Title id="profileHeader" className="text-center">
@@ -11,23 +15,28 @@ const  Profile= () => {
                             <img id="employPhoto" src="https://res.cloudinary.com/dkbwmuo7n/image/upload/v1679689850/sierri.jpg" alt="" />
                         </div>
                         <div id="detailsContainer">
-                            <div id="nameDetails">
-                                <p>Nombre: Maria Pepote Popeye</p>
-                            </div>
-                            <div>
-                                <p>Estatus actual: Habilitado</p>
-                            </div>
-                            <div>
-                                <p>Tipo de Contrato: Temporal</p>
-                            </div>
+                            <Form className="mainDetails">    
+                                <Form.Group id="detailsHeader" className="mb-3" controlId="employeeDni">
+                                    <Form.Label id="detailTitle">Nombre:</Form.Label>
+                                    <Form.Label id="fieldContent">María Pepote Popeye</Form.Label>
+                                </Form.Group>
+                                <Form.Group id="detailsHeader" className="mb-3" controlId="employeeDni">
+                                    <Form.Label id="detailTitle">Estatus actual:</Form.Label>
+                                    <Form.Label id="fieldContent">Activo</Form.Label>
+                                </Form.Group>
+                            </Form> 
                         </div>
                     </Card.Title>
                 <Card.Body id="profileDetails">
                 <Form>    
                     <Form.Group id="detailFields" className="mb-3" controlId="employeeDni">
-                        <Form.Label id="fieldTitle">DNI/NIE</Form.Label>
+                        <Form.Label id="fieldTitle">DNI/NIE:</Form.Label>
                         <Form.Label id="fieldContent">25587562P</Form.Label>
                     </Form.Group>
+                    <Form.Group id="detailFields" className="mb-3" controlId="employeeDni">
+                                    <Form.Label id="fieldTitle">Tipo de Contrato:</Form.Label>
+                                    <Form.Label id="fieldContent">Temporal</Form.Label>
+                                </Form.Group>
                     <Form.Group id="detailFields" className="mb-3" controlId="employeeDni">
                         <Form.Label id="fieldTitle">Sector</Form.Label>
                         <Form.Label id="fieldContent">Jardinería</Form.Label>
