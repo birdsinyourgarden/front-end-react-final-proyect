ce1d222207fdbcbbba62672c9c8751ff98b80bde
ZLoln <lolanavalo@gmail.com>
2023-03-27 19:57:38 +0200

    responsive implemented

--- src/views/profile/Profile.jsx
+++ src/views/profile/Profile.jsx
@@ -38,27 +38,32 @@ const  Profile= () => {
                                     <Form.Label id="fieldContent">Temporal</Form.Label>
                                 </Form.Group>
                     <Form.Group id="detailFields" className="mb-3" controlId="employeeDni">
-                        <Form.Label id="fieldTitle">Sector</Form.Label>
+                        <Form.Label id="fieldTitle">Sector:</Form.Label>
                         <Form.Label id="fieldContent">Jardinería</Form.Label>
                     </Form.Group>    
                     <Form.Group id="detailFields" className="mb-3" controlId="employeeStarting">
-                        <Form.Label id="fieldTitle">Inicio del Contrato</Form.Label>
+                        <Form.Label id="fieldTitle">Inicio del Contrato:</Form.Label>
                         <Form.Label id="fieldContent">01/01/2023</Form.Label>
-                        <Form.Label id="fieldTitle">Fin del Contrato</Form.Label>
+                        <Form.Label id="fieldTitle">Fin del Contrato:</Form.Label>
                         <Form.Label id="fieldContent">01/05/2023</Form.Label>
                     </Form.Group>   
                     <Form.Group id="detailFields" className="mb-3" controlId="employeeEmail">
-                        <Form.Label id="fieldTitle">Correo</Form.Label>
+                        <Form.Label id="fieldTitle">Email:</Form.Label>
                         <Form.Label id="fieldContent">jdjd@gg.com</Form.Label>
                     </Form.Group>
                     <Form.Group id="detailFields" className="mb-3" controlId="password">
-                        <Form.Label id="fieldTitle">Contraseña</Form.Label>
+                        <Form.Label id="fieldTitle">Contraseña:</Form.Label>
                         <Form.Label id="fieldContent">kdkjlds_2255</Form.Label>
                     </Form.Group>
                     <Form.Group id="detailFields" className="mb-3" controlId="employeePhone">
-                        <Form.Label id="fieldTitle">Telefono</Form.Label>
+                        <Form.Label id="fieldTitle">Teléfono:</Form.Label>
                         <Form.Label id="fieldContent">665955558</Form.Label>
                     </Form.Group>
+                    <div className="btnRight">
+                    <Button id="editBtn" variant="primary" type="submit">
+                        Editar
+                    </Button>
+                    </div>
                 </Form>
                 </Card.Body>
             </Card>
