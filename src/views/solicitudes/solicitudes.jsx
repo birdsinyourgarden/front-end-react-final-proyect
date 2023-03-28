eea2b7891f0a6e974d778c53966dbaafcf87dd7d
ZLoln <lolanavalo@gmail.com>
2023-03-28 23:45:08 +0200

    working on solicitude view

--- src/views/solicitudes/solicitudes.jsx
+++ src/views/solicitudes/solicitudes.jsx
@@ -1,11 +1,19 @@
-
+import "../solicitudes/Solicitudes.css";
 
 function Solicitudes(){
 
     return (
-        <div>
-        
-        </div>
+        <div className="solicitudeWrapper">
+            <div>
+                <p className="solicitudeTitle">Gestión de Solicitudes</p>
+            </div>
+
+            <div id="sectionBground">
+                <div>
+                    <p className="processingTitle">Solicitud en Proceso</p>
+                </div>
+            </div>
+        </div>        
     )
 }
 
