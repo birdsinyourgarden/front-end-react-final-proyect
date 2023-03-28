dc46b91c1e9bc040bfbf862142e017ccc13626a4
ZLoln <lolanavalo@gmail.com>
2023-03-28 13:01:22 +0200

    footer amended

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,9 +3,9 @@ import '../footer/Footer.css';
 
 function Footer() {
   return (
-    <Card id="foot">
+    <Card >
       <footer>
-        <div className="d-flex justify-content-between align-items-center">
+        <div id="foot" className="d-flex justify-content-between align-items-center">
           <img src="/Copyright.png" alt="Copyright"></img>
           <img src="/femCoders.png" alt="logo Fem Coders"></img>
         </div>
