19f13b86c3866573424f5cfb56d6c8a33f8c23bf
ZLoln <lolanavalo@gmail.com>
2023-03-29 10:36:50 +0200

    solicitude layout completed, working in responsive

--- src/views/solicitudes/solicitudes.jsx
+++ src/views/solicitudes/solicitudes.jsx
@@ -1,3 +1,4 @@
+import Card from 'react-bootstrap/Card';
 import "../solicitudes/Solicitudes.css";
 
 function Solicitudes(){
@@ -10,8 +11,66 @@ function Solicitudes(){
 
             <div id="sectionBground">
                 <div>
-                    <p className="processingTitle">Solicitud en Proceso</p>
+                    <p className="processingTitle">Solicitudes en Proceso</p>
                 </div>
+                <div id="processingLayout">
+                    <Card id="processingSolicitudeCard">
+                        <div id="processingSolicitudeContainer" >
+                            <div id="solicitudText">
+                                <p>Su solicitud del 5 de Abril del 2023 al 10 de Abril del 2023 está pendiente de gestionar</p>
+                            </div> 
+                        </div>
+                    </Card>
+                    <div>
+                        <button id="manageBtn" type="submit" className="btn btn-link">
+                            Gestionar
+                        </button>
+                    </div> 
+                </div>
+
+                <div>
+                    <p className="rejectingTitle">Solicitudes Denegadas</p>
+                </div>
+                <div id="rejectingLayout">
+                    <Card id="rejectingSolicitudeCard">
+                        <div id="rejectingSolicitudeContainer" >
+                            <div id="solicitudText">
+                                <p>Su solicitud del 1 de Abril del 2023 al 10 de Abril del 2023 ha sido denegada</p>
+                            </div> 
+                        </div>
+                    </Card>
+                </div>    
+
+                <div>
+                    <p className="approvedHolidayTitle">Vacaciones Aprobadas</p>
+                </div>
+                <div id="approvedHolidayLayout">
+                    <Card id="approvedHolidayCard">
+                        <div id="approvedHolidayContainer" >
+                            <div id="solicitudText">
+                                <p>Su solicitud del 1 de Abril del 2023 al 2 de Abril del 2023 ha sido aprobada</p>
+                            </div> 
+                        </div>
+                    </Card>
+                </div>    
+
+                <div>
+                    <p className="approvedAbsenceTitle">Ausencia Aprobada</p>
+                </div>
+                <div id="approvedAbsenceLayout">
+                    <Card id="approvedAbsenceCard">
+                        <div id="approvedAbsenceContainer" >
+                            <div id="solicitudText">
+                                <p>Su solicitud del 1 de Marzo del 2023 al 2 de Marzo del 2023 ha sido aprobada</p>
+                            </div> 
+                        </div>
+                    </Card>
+                    <div>
+                        <button id="editAbsenceBtn" type="submit" className="btn btn-link">
+                            Editar
+                        </button>
+                    </div> 
+                </div>        
             </div>
         </div>        
     )
