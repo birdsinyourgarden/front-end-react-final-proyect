dcd016c2ef43e598f85427bfda2a095e71f3e05c
birdsinyourgarden <ruizpaloma9@gmail.com>
2023-03-29 22:47:47 +0200

    clean code

--- src/components/localNavbar/LocalNavbar.test.jsx
+++ src/components/localNavbar/LocalNavbar.test.jsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom'
 import LocalNavbar from './LocalNavbar';
 
 describe('Local Navbar', () => {
